6897877f443cbce89281e31a5e9bcaeee9e9c8ed
Calvin Jay Ross <calvinjayross@gmail.com>
2020-06-30 00:14:16 -0500

    added step rating function

--- algosim/rating.py
+++ algosim/rating.py
@@ -14,3 +14,12 @@ class RaterFunction(Function):
             lambda p: scale / (1 + np.exp(alpha*((time - p.time) - offset))),
             promotions
         ))
+
+    @classmethod
+    def step(cls, time, promotions):
+        width, = cls.settings(["width"])
+        scale = cls.config.get("scale", 1)
+        return sum(map(
+            lambda p: scale if time - p.time < width else 0,
+            promotions
+        ))
